b6a2e46c1e8c71a197a1940916b7388824b4bea1
RathnaSri Balina <95462105+SriBalina@users.noreply.github.com>
2023-12-31 02:24:36 +0530

    Update and rename 3_Split.py to 4_Split.py

--- pages/4_Split.py
+++ pages/4_Split.py
@@ -1,28 +1,37 @@
 import streamlit as st
-from pymongo import MongoClient
-from st_pages import Page, show_pages, hide_pages
+from collections import defaultdict
+import streamlit as st
+import pymongo
+from collections import defaultdict
 
-# Connect to MongoDB
-client = MongoClient("mongodb+srv://lalithaveni16:lIuZaky5tXTytPN7@cluster0.3b4gvq9.mongodb.net/")
+
+        
+mongo_username = st.secrets.secrets.mongo_username
+mongo_password = st.secrets.secrets.mongo_password
+
+# MongoDB connection URI
+mongo_uri = f"mongodb+srv://{mongo_username}:{mongo_password}@cluster0.bf0tukv.mongodb.net/test?retryWrites=true&w=majority"
+client = pymongo.MongoClient(mongo_uri)
 db = client["user_authentication"]
 balances_collection = db["balances"]
 
-
 def store_balances(username, group_name, transactions):
-    # Store or update transactions in MongoDB
-    balances_collection.update_one(
-        {"username": username},
-        {"$push": {"transactions": {"group_name": group_name, "transactions": transactions}}},
-        upsert=True
-    )
+    try:
+        # Store or update transactions in MongoDB if username is provided
+        if username:
+            balances_collection.update_one(
+                {"username": username},
+                {"$push": {"transactions": {"group_name": group_name, "transactions": transactions}}},
+                upsert=True
+            )
+            st.success("Balances stored successfully!")
+    except Exception as e:
+        st.error(f"An error occurred while storing balances: {str(e)}")
 
-def retrieve_all_transactions(username):
-    # Retrieve all transactions for a user from MongoDB
-    user_data = balances_collection.find_one({"username": username})
-    return user_data.get("transactions", []) if user_data else []
+
+
 
 def splitwise_calculator(group):
-    # ... (rest of your code)
     # Calculate the total amount spent by each person
     total_spent = {member: 0 for member in group["members"]}
     
@@ -42,42 +51,58 @@ def splitwise_calculator(group):
             if consumer != payer:
                 balances[consumer] += amount_paid / len(consumers)
                 transactions.append((consumer, payer, amount_paid / len(consumers)))
-
+    print(transactions)
+    
     return transactions
+
+
+
+
+def simplify_transactions(transactions):
+    balances = defaultdict(float)
+
+    # Calculate net balance for each person
+    for debtor, creditor, amount in transactions:
+        balances[debtor] -= amount
+        balances[creditor] += amount
+
+    simplified_transactions = []
+
+    while True:
+        # Find the person with the maximum positive balance (creditor)
+        creditor = max(balances, key=balances.get)
+        
+        # Find the person with the maximum negative balance (debtor)
+        debtor = min(balances, key=balances.get)
+
+        # If either debtor or creditor has a zero balance, break the loop
+        if balances[creditor] == 0 or balances[debtor] == 0:
+            break
+
+        # Calculate the amount to be settled
+        settled_amount = min(abs(balances[creditor]), abs(balances[debtor]))
+
+        # Update balances and add the transaction to the result
+        balances[creditor] -= settled_amount
+        balances[debtor] += settled_amount
+        simplified_transactions.append((debtor, creditor, settled_amount))
+
+    return simplified_transactions
+
 
 
 # Streamlit app
 st.title("Splitwise Group Expenses")
-st.write(f"Welcome, {st.session_state.username}!")
 
-
-
-# Retrieve username and group name from session state
-username = st.session_state.username
-
-## Button to show previous transactions
-if st.button("Show Previous Transactions"):
-    previous_transactions = retrieve_all_transactions(username)
-    st.write("### Previous Transactions:")
-    for group_transaction in previous_transactions:
-        group_name = group_transaction.get("group_name", "")
-        transactions_list = group_transaction.get("transactions", [])
-        st.write(f"Group: {group_name}")
-        for transaction in transactions_list:
-            st.write(transaction)
-        
-
+# Create or Load a group
 group_name = st.text_input("Enter Group Name:")
 group_key = f"group_{group_name.lower().replace(' ', '_')}"
 group = st.session_state.get(group_key, {"members": [], "expenses": []})
 
-
-
-# ... (rest of your code)
 # Add members to the group
 st.header("Group Members")
 new_member = st.text_input("Add a new member:")
-if st.button("Add Member"):
+if st.button("Add Member") and new_member.strip() != "":
     group["members"].append(new_member.strip())
     st.session_state[group_key] = group
 
@@ -97,17 +122,15 @@ if st.button("Record Expense") and amount_paid > 0 and consumers:
     group["expenses"].append(expense)
     st.session_state[group_key] = group
 
-
-
 # Calculate and display the result when the "Calculate" button is clicked
 if st.button("Calculate Splitwise"):
     transactions = splitwise_calculator(group)
-
-    # Store balances in MongoDB
-    store_balances(username, group_name, transactions)
-
     st.write("### Transactions:")
     for transaction in transactions:
         debtor, creditor, amount = transaction
         st.write(f"{debtor} pay Rs.{amount:.2f} to {creditor}")
-
+    try:
+        username = st.session_state.username
+        store_balances(username, group_name, transactions)
+    except Exception as e:
+        st.warning(f"You are not logged in. Please log in to store details.")   
